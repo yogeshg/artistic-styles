533ca3f46353a4a0d8bed816061b93e0a49ff1d8
Yogesh Garg <yg2482@columbia.edu>
2016-10-27 02:27:45 +0000

    running for 1,3,5,7 layers for 500 neurons each

--- hw/src/run.sh
+++ hw/src/run.sh
@@ -3,11 +3,11 @@
 set -x # echo all commands executed
 
 mkdir -p logs
-
-for layers in $(seq 1 10)
+TS=$(date "+%Y%m%d-%H%M%S")
+for layers in $(seq 1 2 7)
 do
     date
-    TS=$(date "+%Y%m%d-%H%M%S")
-    python hw2b.py --n_layers $layers --n_hidden 500 -e1000 &> logs/run-${layers}x500-${TS}.log
+    hidden=500
+    python hw2b.py --n_layers $layers --n_hidden $hidden -e1000 -v &> logs/run-${layers}x${hidden}-${TS}.log
 done
 
