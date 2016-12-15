376309f3a2bfc498ab36f9077dd71a36be3e54ff
fmarco1 <fmarcogliese@gmail.com>
2016-12-15 15:11:41 -0500

    Fixed apply style

--- src/apply_style.py
+++ src/apply_style.py
@@ -9,7 +9,7 @@ import theano.tensor as T
 
 def train_style(alpha, beta, content_image_path, style_image_path, blank_image_path,
                 style_layers = ['conv1_1','conv2_1','conv3_1','conv4_1','conv5_1'],
-                content_layer = ['conv4_2'], n_epochs=10):
+                content_layer = 'conv4_2', n_epochs=10):
 
     rng = np.random.RandomState(23455)
 
