25f397eef26f4b88bd61a2e5fb75521113d3e55e
fmarco1 <fmarcogliese@gmail.com>
2016-12-15 15:09:05 -0500

    Changed typo in total loos

--- src/apply_style.py
+++ src/apply_style.py
@@ -7,12 +7,10 @@ import numpy as np
 import theano
 import theano.tensor as T
 
-#test_images/starry_night_google.jpg'
-#'test_images/thais.JPG'
-
 def train_style(alpha, beta, content_image_path, style_image_path, blank_image_path,
                 style_layers = ['conv1_1','conv2_1','conv3_1','conv4_1','conv5_1'],
                 content_layer = ['conv4_2'], n_epochs=10):
+
     rng = np.random.RandomState(23455)
 
     print 'loading parameters...'
@@ -53,5 +51,10 @@ def train_style(alpha, beta, content_image_path, style_image_path, blank_image_p
         print (loss)
 
     return loss
+
+
+train_style(0.5, 0.5, 'test_images/thais.JPG', 'test_images/starry_night_google.jpg', 'test_images/whitenoise.jpeg',
+                style_layers = ['conv1_1','conv2_1','conv3_1','conv4_1','conv5_1'],
+                content_layer = ['conv4_2'], n_epochs=10)
 
 
