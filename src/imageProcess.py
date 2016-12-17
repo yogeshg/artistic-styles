f691771f732ddf1a481727cc90aeda07b433ea20
Richard Godden <goddenrich@gmail.com>
2016-12-17 14:50:15 -0500

    deprocessing image bigs fixed

--- src/imageProcess.py
+++ src/imageProcess.py
@@ -48,14 +48,16 @@ def preprocess_image(paths,resize=True):
 def deprocess_image(image_array):
     # put channels last
     assert image_array.ndim==4
-    #image_array = numpy.rollaxis(image_array,1,4)
+    image_array = numpy.rollaxis(image_array,1,4)
+    print image_array.shape
     #BGR -> RGB
-    image_array = image_array[:,::-1, :, :]
+    image_array = image_array[:,:,:,::-1]
+    print image_array.shape
     #add mean channel
     image_array_copy = numpy.zeros(image_array.shape)
-    image_array_copy[:,:,:,0] = image_array[:,0, :, :] + 103.939
-    image_array_copy[:,:,:,1] = image_array[:,1, :, :] + 116.779
-    image_array_copy[:,:,:,2] = image_array[:,2, :, :] + 123.68
+    image_array_copy[:,:,:,0] = image_array[:,:,:,0] + 103.939
+    image_array_copy[:,:,:,1] = image_array[:,:,:,1] + 116.779
+    image_array_copy[:,:,:,2] = image_array[:,:,:,2] + 123.68
     #convert to int between 0 and 254
     image_array_copy = numpy.clip(image_array_copy, 0, 255).astype('uint8')
     return image_array_copy
