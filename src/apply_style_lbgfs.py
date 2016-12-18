0c6c4f6d4e040855b5550f5954b71b42a1fb47b8
fmarco1 <fmarcogliese@gmail.com>
2016-12-18 03:16:35 -0500

    come on lbgfs

--- src/apply_style_lbgfs.py
+++ src/apply_style_lbgfs.py
@@ -122,7 +122,7 @@ def train_style(alpha, beta, content_image_path, style_image_path, blank_image_p
 
     for i in range(n_epochs):
         print(i)
-        new_im, losses, temp = scipy.optimize.fmin_l_bfgs_b(loss_fct_py, numpy.zeros((1,3*224*224)), fprime=grad_fct_py, maxfun=40)
+        new_im, losses, temp = scipy.optimize.fmin_l_bfgs_b(loss_fct_py, np.zeros((1,3*224*224)), fprime=grad_fct_py, maxfun=40)
         #new_im, losses, temp = scipy.optimize.minimize(loss_fct_py, x0.flatten(), args=(), method='L-BFGS-B', jac=grad_fct_py, bounds=None, tol=None, callback=None,
         #                        options={'disp': None, 'maxls': 20, 'iprint': -1, 'gtol': 1e-05, 'eps': 1e-08,
         #                                 'maxiter': 15000, 'ftol': 2.220446049250313e-09, 'maxcor': 10,
